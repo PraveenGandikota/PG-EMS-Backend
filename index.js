5f21d982e1d2267e32d84b359902b157308cc1f2
Praveen <praveengandikota2222@gmail.com>
2025-03-01 10:42:27 +0530

    added a env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://employee-management-system-applicatio.netlify.app/',
+    origin: 'https://employee-management-system-applicatio.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
