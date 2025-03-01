16532f637d0b06f46053d24f003b0539cc2d395c
Praveen <praveengandikota2222@gmail.com>
2025-03-01 10:15:47 +0530

    Backend committing

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"; 
+import { adminRouter } from "./Routes/AdminRoute.js";
+import Jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
+dotenv.config();  
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/auth', adminRouter);
+app.use(express.static('Public'));
+
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+        Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
+            if (err) return res.json({ Status: false, Error: "Wrong Token" });
+            req.id = decoded.id;
+            req.role = decoded.role;
+            next();
+        });
+    } else {
+        return res.json({ Status: false, Error: "Not authenticated" });
+    }
+};
+
+app.get('/verify', verifyUser, (req, res) => {
+    return res.json({ Status: true, role: req.role, id: req.id });
+});
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
